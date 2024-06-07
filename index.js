843659e318fb2974040457348290ec9c9e8909f9
Harshit Kumar <77878114+Harshit-dot-com@users.noreply.github.com>
2024-06-07 21:47:06 +0530

    changes

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const userRoute = require('./routes/userRoute');
 app.use('/',userRoute);
